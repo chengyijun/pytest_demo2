db02ca0cf93e4e62bd08d5448e3557836d4f0415
chengyijun <cyjmmy@foxmail.com>
2020-10-20 15:50:13 +0800

    新增用例优先级用法展示

--- cases/test_allure2.py
+++ cases/test_allure2.py
@@ -1,11 +1,24 @@
 import allure
 import pytest
 
+"""
+BLOCKER = 'blocker'　　  # 阻塞缺陷
+CRITICAL = 'critical'　  # 严重缺陷
+NORMAL = 'normal'　　    # 一般缺陷
+MINOR = 'minor'　　      # 次要缺陷
+TRIVIAL = 'trivial'　　  # 轻微缺陷
+"""
+
 
 # feature() 给功能模块取个名字 一个功能模块包含多个功能点 一般一个功能模块就作为一个类
+
 @allure.feature('购物车功能')
 class TestShoppingTrolley:
+    # 定义用例级别
+    severitis = ['blocker', 'critical', 'normal', 'minor', 'trivial']
+
     # story() 给功能点取个名字 一般一个功能点就作为一个方法
+    @allure.severity(severitis[1])
     @allure.story('加入购物车')
     def test_add_shopping_trolley(self):
         # 步骤1  因为login() 被装饰器 @allure.step()装饰过 报告中会将步骤详细展示出来
@@ -28,10 +41,12 @@ class TestShoppingTrolley:
             allure.attach('添加购物车失败', '实际结果')
             assert 'success' == 'failed'
 
+    @allure.severity(severitis[0])
     @allure.story('修改购物车')
     def test_edit_shopping_trolley(self):
         pass
 
+    @allure.severity(severitis[2])
     @pytest.mark.skipif(reason='本次不执行')
     @allure.story('删除购物车中商品')
     def test_delete_shopping_trolley(self):
